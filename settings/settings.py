55cb949247a36f093f2fa6eb7fea3118a2e9ad44
jacobdenobel <jacobdenobel@gmail.com>
2025-05-03 13:24:33 +0200

    prod

--- settings/settings.py
+++ settings/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9)&1lm@5kri0tg%$do9!2mth=3a%u145!_mi9696x#3)(q*g)9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "jacobdenobel.nl",
+]
 
 
 # Application definition
@@ -122,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
